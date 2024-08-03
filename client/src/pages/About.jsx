4063b4b0c1c366a750e667b74b9cbf0a61f081f3
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-08-04 01:02:48 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About CGS Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        CGS Estate is an innovative online real estate marketplace designed to
+        simplify the process of buying, selling, and renting properties. Our
+        platform connects buyers, sellers, and renters with a comprehensive
+        database of listings in prime locations. At CGS Estate, we are committed
+        to offering a seamless and efficient experience, leveraging cutting-edge
+        technology to meet the diverse needs of our clients.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission at CGS Estate is to empower our clients by providing
+        unparalleled service, expert guidance, and an in-depth understanding of
+        the real estate market. Whether you are a first-time homebuyer, a
+        seasoned investor, or someone looking for a rental property, our
+        user-friendly platform and dedicated support team are here to assist you
+        at every step of your real estate journey.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With a team of experienced professionals, CGS Estate combines industry
+        expertise with a passion for helping clients achieve their real estate
+        goals. We believe that finding the perfect property should be an
+        enjoyable and rewarding experience, and we strive to make that a reality
+        for each of our users. Trust CGS Estate to be your reliable partner in
+        navigating the dynamic world of real estate.
+      </p>
+    </div>
+  );
 }
