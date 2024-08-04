605b8a7ce611a947f05042661fdde8affd80abad
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-08-05 02:09:39 +0530

    chore: Update Vite configuration to use environment variables for API proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: `${import.meta.env.VITE_BASE_URL}`,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BASE_URL,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
